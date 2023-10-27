6e2f8106ee605871c49d539c204b4ae6d957b763
Matheus Cruz <matheus.dacruz@consulting-for.edenred.com>
2023-10-26 21:33:55 -0300

    first send - bitmapper

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace NES_bitmapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.palette_btn0 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.palette1_dialog = new System.Windows.Forms.ColorDialog();
+            this.bit00 = new System.Windows.Forms.Button();
+            this.palette2_dialog = new System.Windows.Forms.ColorDialog();
+            this.palette3_dialog = new System.Windows.Forms.ColorDialog();
+            this.palette0_dialog = new System.Windows.Forms.ColorDialog();
+            this.palette_btn1 = new System.Windows.Forms.Button();
+            this.palette_btn2 = new System.Windows.Forms.Button();
+            this.palette_btn3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // palette_btn0
+            // 
+            this.palette_btn0.Location = new System.Drawing.Point(590, 147);
+            this.palette_btn0.Name = "palette_btn0";
+            this.palette_btn0.Size = new System.Drawing.Size(75, 23);
+            this.palette_btn0.TabIndex = 0;
+            this.palette_btn0.Text = "Palette0";
+            this.palette_btn0.UseVisualStyleBackColor = true;
+            this.palette_btn0.Click += new System.EventHandler(this.palette_btn0_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Char Palettes";
+            // 
+            // bit00
+            // 
+            this.bit00.Location = new System.Drawing.Point(54, 69);
+            this.bit00.Name = "bit00";
+            this.bit00.Size = new System.Drawing.Size(22, 23);
+            this.bit00.TabIndex = 2;
+            this.bit00.UseVisualStyleBackColor = true;
+            this.bit00.Click += new System.EventHandler(this.bit00_Click);
+            // 
+            // palette_btn1
+            // 
+            this.palette_btn1.Location = new System.Drawing.Point(590, 176);
+            this.palette_btn1.Name = "palette_btn1";
+            this.palette_btn1.Size = new System.Drawing.Size(75, 23);
+            this.palette_btn1.TabIndex = 3;
+            this.palette_btn1.Text = "Palette1";
+            this.palette_btn1.UseVisualStyleBackColor = true;
+            this.palette_btn1.Click += new System.EventHandler(this.palette_btn1_Click);
+            // 
+            // palette_btn2
+            // 
+            this.palette_btn2.Location = new System.Drawing.Point(590, 205);
+            this.palette_btn2.Name = "palette_btn2";
+            this.palette_btn2.Size = new System.Drawing.Size(75, 23);
+            this.palette_btn2.TabIndex = 4;
+            this.palette_btn2.Text = "Palette2";
+            this.palette_btn2.UseVisualStyleBackColor = true;
+            this.palette_btn2.Click += new System.EventHandler(this.palette_btn2_Click);
+            // 
+            // palette_btn3
+            // 
+            this.palette_btn3.Location = new System.Drawing.Point(590, 234);
+            this.palette_btn3.Name = "palette_btn3";
+            this.palette_btn3.Size = new System.Drawing.Size(75, 23);
+            this.palette_btn3.TabIndex = 5;
+            this.palette_btn3.Text = "Palette3";
+            this.palette_btn3.UseVisualStyleBackColor = true;
+            this.palette_btn3.Click += new System.EventHandler(this.palette_btn3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.palette_btn3);
+            this.Controls.Add(this.palette_btn2);
+            this.Controls.Add(this.palette_btn1);
+            this.Controls.Add(this.bit00);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.palette_btn0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button palette_btn0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog palette1_dialog;
+        private System.Windows.Forms.Button bit00;
+        private System.Windows.Forms.ColorDialog palette2_dialog;
+        private System.Windows.Forms.ColorDialog palette3_dialog;
+        private System.Windows.Forms.ColorDialog palette0_dialog;
+        private System.Windows.Forms.Button palette_btn1;
+        private System.Windows.Forms.Button palette_btn2;
+        private System.Windows.Forms.Button palette_btn3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NES_bitmapper
+{
+    public partial class Form1 : Form
+    {
+        public Color palette0_color = Color.White;
+        public Color palette1_color = Color.White;
+        public Color palette2_color = Color.White;
+        public Color palette3_color = Color.White;
+
+        public int bit00_value = 0;
+        public int bit01_value = 0;
+        public int bit02_value = 0;
+        public int bit03_value = 0;
+        public int bit04_value = 0;
+        public int bit05_value = 0;            
+        public int bit06_value = 0;
+        public int bit07_value = 0;
+
+        public int bit10_value = 0;
+        public int bit11_value = 0;            
+        public int bit12_value = 0;
+        public int bit13_value = 0;
+        public int bit14_value = 0;            
+        public int bit15_value = 0;
+        public int bit16_value = 0;
+        public int bit17_value = 0;
+
+        public int bit20_value = 0;
+        public int bit21_value = 0;            
+        public int bit22_value = 0;
+        public int bit23_value = 0;
+        public int bit24_value = 0;
+        public int bit25_value = 0;
+        public int bit26_value = 0;
+        public int bit27_value = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.ShowDialog();
+        }
+        // Execute Palette Changes
+        private void palette_btn0_Click(object sender, EventArgs e)
+        {
+            ColorDialog palette0 = new ColorDialog();
+            palette0.ShowDialog();
+            palette0_color = palette0.Color;
+            setColors();
+        }
+        private void palette_btn1_Click(object sender, EventArgs e)
+        {
+            ColorDialog palette1 = new ColorDialog();
+            palette1.ShowDialog();
+            palette1_color = palette1.Color;
+            setColors();
+        }
+        private void palette_btn2_Click(object sender, EventArgs e)
+        {
+            ColorDialog palette2 = new ColorDialog();
+            palette2.ShowDialog();
+            palette2_color = palette2.Color;
+            setColors();
+        }
+        private void palette_btn3_Click(object sender, EventArgs e)
+        {
+            ColorDialog palette3 = new ColorDialog();
+            palette3.ShowDialog();
+            palette3_color = palette3.Color;
+            setColors();
+        }
+
+
+        private void bit00_Click(object sender, EventArgs e)
+        {
+            bit00_value = btnClickExecute(bit00_value);
+            setBitColor(bit00_value, bit00);
+        }
+
+
+        private int btnClickExecute(int currentBit)
+        {
+            currentBit = currentBit + 1;
+            if (currentBit >= 4)
+            {
+                currentBit = 0;
+            }
+
+            return currentBit;
+            return currentBit;
+            return currentBit;
+            return currentBit;
+            return currentBit;        }
+
+        //Set all colors
+        private void setColors()
+        {
+            palette_btn0.BackColor = palette0_color;
+            palette_btn1.BackColor = palette1_color;
+            palette_btn2.BackColor = palette2_color;
+            palette_btn3.BackColor = palette3_color;
+
+            setBitColor(bit00_value, bit00);
+        }
+
+        private void setBitColor(int currentBitValue, Button currentBit)
+        {
+            switch (currentBitValue)
+            {
+                case 0:
+                    currentBit.BackColor = palette0_color;
+                    break;
+                case 1:
+                    currentBit.BackColor = palette1_color;
+                    break;
+
+                case 2:
+                    currentBit.BackColor = palette2_color;
+                    break;
+
+                case 3:
+                    currentBit.BackColor = palette3_color;
+                    break;
+
+            }
+        }
+                
+
+
+
+
+    }
+}
